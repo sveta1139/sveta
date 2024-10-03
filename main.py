1133f6b6ec04f67cf14ae73c4297131f429d0449
sveta1139 <39svetochka39@gmail.com>
2024-10-03 15:58:51 +0000

    лекция1

---
+++ main.py
@@ -0,0 +1,12 @@
+print('Hello,World!')
+print(type('Hello world'))
+print('Маша + Петя = Любовь')
+print(type('Маша = Петя = Любовь'))
+a=3
+s=4
+x=a+s
+print(x)
+print(type(x))
+print('x=', 3+4)
+print(type('x=', 3+4))
+
